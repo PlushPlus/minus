88309ba09636d9f80c4056c8dac742a53e790769
Plush <141276987+PlushPlus@users.noreply.github.com>
2025-01-25 00:41:34 +0100

    e

---
+++ popup.js
@@ -0,0 +1,54 @@
+let url = "";
+let username = "";
+let avatar_url = "";
+let msg = "";
+
+function sendToWebhook(data) {
+    const webhookUrl = url;
+
+    fetch(webhookUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(result => console.log('Success:', result))
+    .catch(error => console.log('Error:', error));
+}
+
+document.getElementById("button").addEventListener('click',function(ev) {
+    const urlTextBox = document.getElementById("url-text-box");
+    const urltext = urlTextBox.value;
+    url = urltext;
+
+    const msgTextBox = document.getElementById("msg-text-box");
+    const msgtext = msgTextBox.value;
+    msg = msgtext;
+
+    const usernameTextBox = document.getElementById("name-text-box");
+    const usernametext = usernameTextBox.value;
+    username = usernametext;
+
+    const avatarUrlTextBox = document.getElementById("pfp-text-box");
+    const avatarUrltext = avatarUrlTextBox.value;
+    avatar_url = avatarUrltext;
+
+    const payload = {
+        content: msg,
+        username: username,
+        avatar_url: avatar_url,
+        /*embeds: [{
+            title: 'title',
+            description: 'desc',
+            color: 3447003
+        }]*/
+    };
+    
+    sendToWebhook(payload);
+});
+
+document.getElementById("url-text-box").addEventListener("click",function(ev){
+
+});
